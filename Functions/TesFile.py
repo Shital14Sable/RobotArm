75da955fd1ed3d34ac08a143fa9d5089e97b13b0
Shital14Sable <shital14sable@gmail.com>
2021-06-05 05:10:05 +0800

    Test File changed

--- Functions/TesFile.py
+++ Functions/TesFile.py
@@ -13,69 +13,25 @@ if sys.version_info.major == 2:
     sys.exit(0)
 
 AK = ArmIK()
-
-# 夹持器夹取时闭合的角度
+# The angle at which the gripper is closed when gripping
 servo1 = 500
 
-def initMove():
-    Board.setBusServoPulse(1, servo1 - 50, 500)
-    Board.setBusServoPulse(2, 500, 500)
-    AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, 1500)
-
-__isRunning = False
-def reset():
-    initMove()
-
-def init():
-    global __isRunning
-    reset()
-    __isRunning = True
-    print("Calibration Init")
-
-def start():
-    global __isRunning
-    __isRunning = True
-    print("Calibration Start")
-
-def stop():
-    global __isRunning
-    __isRunning = False
-    print("Calibration Stop")
-
-def exit():
-    global __isRunning
-    __isRunning = False
-    print("Calibration Exit")
-
-def run(img):
-    global __isRunning
-    
-    if not __isRunning:
-        return img
-    
-    img_h, img_w = img.shape[:2]
-    cv2.line(img, (0, int(img_h / 2)), (img_w, int(img_h / 2)), (0, 0, 255), 2)
-    cv2.line(img, (int(img_w / 2), 0), (int(img_w / 2), img_h), (0, 0, 255), 2)
-    return img
-
 if __name__ == '__main__':
-    init()
-    start()
     my_camera = Camera.Camera()
     my_camera.camera_open()
-    while True:
-        img = my_camera.frame
-        if img is not None:
-            frame = img.copy()
-            Frame = run(frame)           
-            cv2.imshow('Frame', Frame)
-            result1 = AK.setPitchRangeMoving((0, 20, 5), -90, -90, 0, 100)
-            print(result1)
-            time.sleep(1)
-            result2 = AK.setPitchRangeMoving((2, 20, 5), -90, -90, 0, 100)
-            print(result2)
-            key = cv2.waitKey(1)
-            if key == 27:
-                break
-
-    
+    Board.setBusServoPulse(1, servo1 - 200, 500)  # Open the paws and put down the object
+    time.sleep(2)
+    Board.setBusServoPulse(1, servo1, 00)  # Holder closed
+    time.sleep(1)
+    result1 = AK.setPitchRangeMoving((-15, 28, 10), -90, -90, 0, 1000)
+    print(result1)
+    time.sleep(2)
+    result2 = AK.setPitchRangeMoving((15, 28, 10), -90, -90, 0, 1000)
+    print(result2)
+    time.sleep(2)
+    result3 = AK.setPitchRangeMoving((15, 12, 10), -90, -90, 0, 1000)
+    print(result3)
+    time.sleep(2)
+    result4 = AK.setPitchRangeMoving((-15, 12, 10), -90, -90, 0, 1000)
+    print(result4)
+    time.sleep(2)
